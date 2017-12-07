be43ceaa0341aa3492c931013d5f8573b2474d7c
ll1126 <30997960+ll1126@users.noreply.github.com>
2017-12-07 15:32:02 +0800

    Update springbootTestApplication.java

--- src/main/java/com/test/springbootTestApplication.java
+++ src/main/java/com/test/springbootTestApplication.java
@@ -7,12 +7,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableCaching    //开启缓存
+// @EnableCaching    //开启缓存
 @SpringBootApplication //springboot 启动注解
 @EnableWebMvc       //mvc启动注解
 @MapperScan("com.test.Mapper")  //mapper扫描
 @EnableTransactionManagement    //开启事务支持  然后在需要的service 加@Transactional    ！！！多个数据源的话要配置多个事务管理器！！！
 public class springbootTestApplication {
+    
+    
     //springboot 启动方法
     public static void main(String[] args){
         SpringApplication.run(springbootTestApplication.class,args);
